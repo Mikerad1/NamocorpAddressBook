f8a39a9265d931128146c829635f23bdcf42e42e
Fluffy <michael.rademeyer@sybrin.com>
2020-10-24 15:25:15 +0200

    made changes

--- AddressBook/Server/Controllers/AddressBookController.cs
+++ AddressBook/Server/Controllers/AddressBookController.cs
@@ -3,12 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using AddressBook.Server.Data;
+using AddressBook.Shared.Model;
+
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AddressBook.Server.Controllers {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class AddressBookController : ControllerBase {
+        private readonly ApplicationDbContext _db;
+        public AddressBookController(ApplicationDbContext db) {
+            _db = db;
+        }
+        [HttpGet]
+        public IEnumerable<Contact> Get() {
+            return _db.Contacts;
+        }
     }
 }
