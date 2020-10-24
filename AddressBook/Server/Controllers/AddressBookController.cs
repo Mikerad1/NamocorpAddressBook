a9a269704e6ff045c1736a98ff7bd51c5b87dee1
Fluffy <michael.rademeyer@sybrin.com>
2020-10-24 15:21:22 +0200

    Added Address Book Controller

---
+++ AddressBook/Server/Controllers/AddressBookController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AddressBook.Server.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressBookController : ControllerBase {
+    }
+}
